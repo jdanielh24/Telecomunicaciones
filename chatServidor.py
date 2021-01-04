262acb02da8c82cce01f6438937e999bda1d949d
Ivan Gonzalez <kikin-gr2000@hotmail.com>
2021-01-03 20:06:03 -0600

    manejo del error de arriba

--- chatServidor.py
+++ chatServidor.py
@@ -1,22 +1,23 @@
 import socket
 import threading
 
+
 class Server:
     def __init__(self):
         self.start_server()
 
     def start_server(self):
-        self.s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
         host = ''
         port = 5555
-		#port = int(input('Enter port to run the server on --> '))
-		
+        #port = int(input('Enter port to run the server on --> '))
+
         self.clients = []
 
-        self.s.bind((host,port))
+        self.s.bind((host, port))
         self.s.listen(100)
-    
+
         print('Running on host: '+str(host))
         print('Running on port: '+str(port))
 
@@ -26,37 +27,43 @@ class Server:
             c, addr = self.s.accept()
 
             username = c.recv(1024).decode()
-            
+
             print('New connection. Username: '+str(username))
             self.broadcast('New person joined the room. Username: '+username)
 
             self.username_lookup[c] = username
 
             self.clients.append(c)
-             
-            threading.Thread(target=self.handle_client,args=(c,addr,)).start()
 
-    def broadcast(self,msg):
+            threading.Thread(target=self.handle_client,
+                             args=(c, addr,)).start()
+
+    def broadcast(self, msg):
         for connection in self.clients:
             connection.send(msg.encode())
 
-    def handle_client(self,c,addr):
+    def handle_client(self, c, addr):
         while True:
             try:
-                msg = c.recv(1024)
+                try:
+                    msg = c.recv(1024)
+                except:
+                    c.shutdown(socket.SHUT_RDWR)
+                    self.clients.remove(c)
+
+                    print(str(self.username_lookup[c])+' left the room.')
+                    self.broadcast(
+                        str(self.username_lookup[c])+' has left the room.')
+
+                    break
+
+                if msg.decode() != '':
+                    print('New message: '+str(msg.decode()))
+                    for connection in self.clients:
+                        if connection != c:
+                            connection.send(msg)
             except:
-                c.shutdown(socket.SHUT_RDWR)
-                self.clients.remove(c)
-                
-                print(str(self.username_lookup[c])+' left the room.')
-                self.broadcast(str(self.username_lookup[c])+' has left the room.')
+                print('error al manejar el error de arriba :v')
 
-                break
-
-            if msg.decode() != '':
-                print('New message: '+str(msg.decode()))
-                for connection in self.clients:
-                    if connection != c:
-                        connection.send(msg)
 
 server = Server()
