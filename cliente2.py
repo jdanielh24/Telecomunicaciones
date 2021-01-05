f5d74b297e9f78eb0a671642df968c6919375ebc
jdanielh24 <jdanielopez24@gmail.com>
2021-01-04 21:22:42 -0600

    Eliminar version 2 de chat

--- cliente2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import socket
-from servidor2 import Chat, server
-
-if __name__ == '__main__':
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    Chat(sock, server)
